a3f8fc666220edb6e4dca650de402c5c6fd838cd
Naman Singh Parihar <87593367+naman-parihar2000@users.noreply.github.com>
2023-06-08 23:51:07 +0530

    added sorting to the table

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,17 +1,19 @@
-import Table from '../components/Table';
+// import Table from '../components/Table';
+import SortableTable from './SortableTable';
 
 function TablePage() {
     const data = [
-        { name: 'Orange', color: 'bg-orange-500', score: 5 },
-        { name: 'Apple', color: 'bg-red-500', score: 3 },
-        { name: 'Banana', color: 'bg-yellow-500', score: 1 },
-        { name: 'Lime', color: 'bg-green-500', score: 4 },
+        { name: 'Orange', color: 'bg-orange-300', score: 5 },
+        { name: 'Apple', color: 'bg-red-300', score: 3 },
+        { name: 'Banana', color: 'bg-yellow-300', score: 1 },
+        { name: 'Lime', color: 'bg-green-300', score: 4 },
     ];
 
     const config = [
         {
             label: 'Name',
             render: (fruit) => fruit.name,
+            sortValue: (fruit) => fruit.name,
         },
         {
             label: 'Color',
@@ -20,6 +22,7 @@ function TablePage() {
         {
             label: 'Score',
             render: (fruit) => fruit.score,
+            sortValue: (fruit) => fruit.score,
         },
     ];
 
@@ -29,7 +32,7 @@ function TablePage() {
 
     return (
         <div>
-            <Table data={data} config={config} keyFn={keyFn} />
+            <SortableTable data={data} config={config} keyFn={keyFn} />
         </div>
     );
 }
